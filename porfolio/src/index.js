be43e53d8acc93341bd0e26d1f1a1d6f605dbcea
yuvrajput12 <ys55724@gmail.com>
2022-12-05 22:59:04 +0530

    second

--- porfolio/src/index.js
+++ porfolio/src/index.js
@@ -2,5 +2,6 @@
 import  ReactDOM  from 'react-dom'
 import App from './App'
 import './index.css'
+import React, { Component }  from 'react';
 
 ReactDOM.render(<App/>,document.querySelector("#root"))
