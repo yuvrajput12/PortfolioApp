4aef76686095fecb8ced0b78367f3047bd44f46b
yuvrajput12 <ys55724@gmail.com>
2022-12-05 23:26:44 +0530

    Updates

---
+++ precache-manifest.8b94f63e76849b5527e87b1759192f04.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "301e8b5870538434bd84",
+    "url": "%7Byuvrajput12%7D.github.io/%7BPortfolioApp%7D/static/css/main.ca657f58.chunk.css"
+  },
+  {
+    "revision": "301e8b5870538434bd84",
+    "url": "%7Byuvrajput12%7D.github.io/%7BPortfolioApp%7D/static/js/main.939c44c2.chunk.js"
+  },
+  {
+    "revision": "80977a05ae822c7462ab",
+    "url": "%7Byuvrajput12%7D.github.io/%7BPortfolioApp%7D/static/js/runtime~main.3d6cc411.js"
+  },
+  {
+    "revision": "4516598a3b0b2327aebe",
+    "url": "%7Byuvrajput12%7D.github.io/%7BPortfolioApp%7D/static/js/2.1b477aa2.chunk.js"
+  },
+  {
+    "revision": "0f534e1378808d5c4143dadb251e833e",
+    "url": "%7Byuvrajput12%7D.github.io/%7BPortfolioApp%7D/static/media/project3.0f534e13.webp"
+  },
+  {
+    "revision": "562834d2a6cdae56ff0170eb7c7aa46c",
+    "url": "%7Byuvrajput12%7D.github.io/%7BPortfolioApp%7D/static/media/project2.562834d2.jpg"
+  },
+  {
+    "revision": "e8d5efb85baf5ea3b196a076fd2ebfc3",
+    "url": "%7Byuvrajput12%7D.github.io/%7BPortfolioApp%7D/static/media/cv.e8d5efb8.pdf"
+  },
+  {
+    "revision": "484f3dcea5a578782d17a09bef1806c2",
+    "url": "%7Byuvrajput12%7D.github.io/%7BPortfolioApp%7D/static/media/photo.484f3dce.png"
+  },
+  {
+    "revision": "85ae863e7f1d71f17f8c49799c8c446f",
+    "url": "%7Byuvrajput12%7D.github.io/%7BPortfolioApp%7D/static/media/about-image.85ae863e.jpg"
+  },
+  {
+    "revision": "2537ce4f430c14cf44ef4380c92b89eb",
+    "url": "%7Byuvrajput12%7D.github.io/%7BPortfolioApp%7D/static/media/project4.2537ce4f.webp"
+  },
+  {
+    "revision": "555c20817e874bcb70e6ce84fd2b1c43",
+    "url": "%7Byuvrajput12%7D.github.io/%7BPortfolioApp%7D/static/media/project5.555c2081.webp"
+  },
+  {
+    "revision": "d704a7aeea2f1203b40f9e16a97d6cb9",
+    "url": "%7Byuvrajput12%7D.github.io/%7BPortfolioApp%7D/static/media/lines-png-16257.d704a7ae.png"
+  },
+  {
+    "revision": "648a4e76e19d7a05423c8b17859e50bb",
+    "url": "%7Byuvrajput12%7D.github.io/%7BPortfolioApp%7D/index.html"
+  }
+];
